47678fd4875caabed8e7cfd48e5cb599cf5b500e
Aditya Gupta <agupta29@wisc.edu>
2013-08-29 20:04:36 -0500

    Added tests to the median function

--- test_stats.py
+++ test_stats.py
@@ -27,6 +27,16 @@ def test_floating_mean1():
 
 # FIXME Put Median tests here
 
+def test_median():
+    obs = median([1,2,3,4,5])
+    exp = 3 
+    assert_equal(obs,exp)
+
+def test_median2():
+    obs = median([1,2,3,4])
+    exp = 2.5
+    assert_equal(obs,exp)
+
 # FIXME Put Mode tests here
 
 def test_std1():
